13230525b620d3c5fbe30c7339d74a95f4cb7736
Leslie Vallognes <leslievallognes85@gmail.com>
2025-10-07 11:32:38 +0200

    Ajout de la page d’erreur 404 avec routage centralisé - Création du composant Error conforme à la maquette - Ajout du layout global avec Header et Footer sur toutes les pages - Mise en place du routage centralisé avec React Router - Redirection automatique vers la page 404 pour toute route inexistante

---
+++ src/Router.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import Layout from "./Layout";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Error.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/error.scss";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <div className="error-container">
+      <h1 className="error-code">404</h1>
+      <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
+    </div>
+  );
+};
+
+export default Error;
